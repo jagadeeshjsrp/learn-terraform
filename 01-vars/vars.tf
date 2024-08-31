44cc259381c8e0ca55fd2a84bf7930fcbf3f16d5
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-01 05:26:37 +0530

    variale terraform

---
+++ 01-vars/vars.tf
@@ -0,0 +1,17 @@
+variable "sample_string" {
+  default = "Hello World"
+}
+
+## Shell Scripting equivalent sample_string="Hello World"
+# To print in shell script we use echo, following is the syntax in shell
+# echo $sample_string
+
+output "sample_string" {
+  value = var.sample_string
+}
+
+# In above case we are just printing just variable, If variable is a combination of some strings then following is the syntax
+# ${} is mandetory if we include the variable inside a string
+output "sample_string1" {
+  value = "value of sample_string = ${var.sample_string}"
+}
