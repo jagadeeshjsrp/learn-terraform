7dee0a554274e2aaefffc977f7e5de76acc9698d
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-03 13:10:49 +0530

    terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -12,8 +12,16 @@ variable "sample1" {
   default = [ "abc", "xyz"]
 }
 
+variable "sample2" {
+  xyz = 100
+  abc = 200
+}
 ## Print first value like var.sample[0] with a function
 
 output "sample1" {
   value = element(var.sample1, 0)
+}
+
+output "sample2" {
+  value = var.sample2["a1"]
 }
