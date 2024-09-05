47c0b2a662c25427d1403c31b1cc5b1772e6f742
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-05 22:35:42 +0530

    Terraform import

--- 07-import/main.tf
+++ 07-import/main.tf
@@ -3,8 +3,6 @@ resource "aws_instance" "workstation" {
   instance_type = "unknown"
 }
 
-resource "aws_instance" "workstation" {}
-
 output "private_ip" {
   value = aws_instance.workstation.private_ip
 }
